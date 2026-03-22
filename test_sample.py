0650a4f2c4b236217597354b811e95242632d1ae
hingwe <sudama.hingwe21@gmail.com>
2026-03-22 16:34:23 +0530

    Update test_sample.py
    Added python test

--- test_sample.py
+++ test_sample.py
@@ -156,6 +156,29 @@ def test_google_title():
         
 #################################################### Sample Playwright Test Case ###########################        
         
+s = "I love Python"
+print(" ".join(s.split()[::-1]))   
+#####Anagram string#########
+s1 = "listen"
+s2 = "silent"
+
+print(sorted(s1) == sorted(s2))
+
+list = [0,1,1,0,3,3,12,12]
+
+
+for i in list:
+    if i==1:
+        list.remove(i)
+        list.append(i)
+print(list)
+########Palindrom
+s = "madam"
+a=121
+
+print(str(a)==str(a)[::-1])
+print(s == s[::-1])       
+                
 
         
                 
